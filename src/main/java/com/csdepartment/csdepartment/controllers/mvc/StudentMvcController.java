7d623e4bdda757e0bd10d58883fa9dc1488f320b
gshutov <g.h.shutov@gmail.com>
2022-05-18 13:47:02 +0300

    Front-end: Created student view for all sorting parameters.

--- src/main/java/com/csdepartment/csdepartment/controllers/mvc/StudentMvcController.java
+++ src/main/java/com/csdepartment/csdepartment/controllers/mvc/StudentMvcController.java
@@ -29,6 +29,30 @@ public class StudentMvcController {
         return "students";
     }
 
+    @GetMapping("last-name")
+    public String showAllStudentsPageLastName(Model model){
+        model.addAttribute("students", studentService.sortByLastName());
+        return "students-order-by-last-name";
+    }
+
+    @GetMapping("year-in-uni")
+    public String showAllStudentsPageYearInUni(Model model){
+        model.addAttribute("students", studentService.sortByYearInUni());
+        return "students-order-by-year-in-uni";
+    }
+
+    @GetMapping("credits")
+    public String showAllStudentsPageCredits(Model model){
+        model.addAttribute("students", studentService.sortByCredits());
+        return "students-order-by-credits";
+    }
+
+    @GetMapping("top-3")
+    public String showTop3Students(Model model){
+        model.addAttribute("students", studentService.top3Students());
+        return "students-order-by-top-3";
+    }
+
     @GetMapping("/new")
     public String getCreateStudentPage(Model model){
         model.addAttribute("createStudentDto", new CreateStudentDto());
