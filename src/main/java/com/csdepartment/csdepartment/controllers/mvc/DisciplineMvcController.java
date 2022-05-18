5e4cdc08287b484bc3b6e069d87f5163c8945744
gshutov <g.h.shutov@gmail.com>
2022-05-19 00:12:26 +0300

    Create discipline working;

--- src/main/java/com/csdepartment/csdepartment/controllers/mvc/DisciplineMvcController.java
+++ src/main/java/com/csdepartment/csdepartment/controllers/mvc/DisciplineMvcController.java
@@ -1,12 +1,17 @@
 package com.csdepartment.csdepartment.controllers.mvc;
 
+import com.csdepartment.csdepartment.models.Discipline;
+import com.csdepartment.csdepartment.models.dto.CreateDisciplineDto;
 import com.csdepartment.csdepartment.models.mappers.DisciplineMapper;
 import com.csdepartment.csdepartment.services.DisciplineService;
 import com.csdepartment.csdepartment.services.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -42,5 +47,26 @@ public class DisciplineMvcController {
     public String top3DisciplinesByStudents(Model model){
         model.addAttribute("disciplines", disciplineService.top3Disciplines());
         return "disciplines-order-by-top-3";
+    }
+
+    @GetMapping("/new")
+    private String getCreateDisciplinePage(Model model){
+        model.addAttribute("CreateDisciplineDto", new CreateDisciplineDto());
+        model.addAttribute("teachers", teacherService.getAll());
+        return "create-discipline";
+    }
+
+    @PostMapping("/new")
+    public String handleCreateDiscipline(@ModelAttribute CreateDisciplineDto dto, BindingResult bindingResult, Model model){
+        Discipline disciplineToCreate = disciplineMapper.fromDto(dto);
+        model.addAttribute("CreateDisciplineDto", new CreateDisciplineDto());
+        model.addAttribute("teachers", teacherService.getAll());
+        if (bindingResult.hasErrors()){
+            return "create-discipline";
+        }
+
+        disciplineService.create(disciplineToCreate);
+
+        return "redirect:/disciplines";
     }
 }
