59088a33e3b196e28cbab14b9a98e5f3dc998769
gshutov <g.h.shutov@gmail.com>
2022-05-19 19:30:09 +0300

    Last commit before sending to Albena.

--- src/main/java/com/csdepartment/csdepartment/CsDepartmentApplication.java
+++ src/main/java/com/csdepartment/csdepartment/CsDepartmentApplication.java
@@ -3,11 +3,13 @@ package com.csdepartment.csdepartment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CsDepartmentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CsDepartmentApplication.class, args);
 	}
+
 
 }
